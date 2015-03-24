292d0bd910650cc1d54b588c08e8a724072b96a9
Allen Clineff <ajclinef@gmail.com>
2015-03-24 16:30:26 -0400

    Rename your user with the /me command

--- index.js
+++ index.js
@@ -2,16 +2,44 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var i = 0;
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/html/index.html');
 });
 
 io.on('connection', function(socket){
+    var username = 'User' + i;
+    i++;
+
+    var user = {};
+    user.name = username;
+
+
+    io.emit('chat message', 'User ' + username + ' Connected!');
+
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        io.emit('chat message', makeMessage(msg, user));
     });
+
+    socket.on('disconnect', function(){
+        io.emit('chat message', 'User ' + user.name + ' Disconnected!');
+        i--;
+    });
+
 });
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
+
+var makeMessage = function(msg, user){
+    if(msg.message.indexOf('/me') == 0){
+        var newName = msg.message.slice(4);
+        var oldName = user.name;
+        user.name = newName;
+        return 'User '+ oldName + ' is now known as ' + user.name;
+    }
+
+    return user.name + " : " + msg.message;
+}
